9cfa52c72ed2bbc5a8acbe76ca63ba98b3aef2ac
bambi teague <teeegsa@gmail.com>
2021-09-13 10:14:14 -0500

    adding createStore and creating empy folders within src folder

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux"
+
+const store = createStore()
 
 ReactDOM.render(
   <React.StrictMode>
