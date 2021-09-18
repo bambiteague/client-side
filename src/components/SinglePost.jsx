95b439a496523bfe2c0f4a0439a9f01d0df07e6d
bambi teague <teeegsa@gmail.com>
2021-09-18 11:26:35 -0500

    work on single post show page (not working yet)

---
+++ src/components/SinglePost.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { useSelector } from "react-redux";
+
+class SinglePost extends Component {
+  render() {
+    SinglePostPage = ({ match }) => {
+      const { postId } = match.params;
+
+      const post = useSelector(({ state: posts }) =>
+        state.posts.find((post) => post.id === postId)
+      );
+
+      if (!post) {
+        return (
+          <section>
+            <h2>Post not found!</h2>
+          </section>
+        );
+      }
+
+      return (
+        <section>
+          <article className="post">
+            <h2>{post.title}</h2>
+            <p className="post-content">{post.content}</p>
+          </article>
+        </section>
+      );
+    };
+  }
+}
+
+export default SinglePost;
--- src/containers/AllPosts.jsx
+++ src/containers/AllPosts.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BrowserRouter as Link } from 'react-router-dom';
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions/fetchPosts";
 
@@ -12,18 +13,24 @@ export class AllPosts extends Component {
       <div>
         <h1>Posts</h1>
         {this.props.requesting ? (
-          <h1>Loading...</h1>   
+          <h1>Loading...</h1>
         ) : (
           this.props.posts.map((post) => (
             <ul>
-            <li key={post.id}> <h3>{post.title}</h3><br/>by: {post.author.name}</li>
-            </ul>   
+              <li key={post.id}>
+                <h3>
+                  <Link to="/posts/{post.id}">
+                    {post.title}
+                  </Link>
+                </h3>
+                by: {post.author.name}
+              </li>
+            </ul>
           ))
         )}
       </div>
     );
   }
-  
 }
 
 const mapStateToProps = ({ postReducer }) => {
@@ -33,4 +40,4 @@ const mapStateToProps = ({ postReducer }) => {
   };
 };
 
-export default connect(mapStateToProps, {fetchPosts})(AllPosts);
+export default connect(mapStateToProps, { fetchPosts })(AllPosts);
